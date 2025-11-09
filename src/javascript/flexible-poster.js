3037e5a455634f084d185da12307ba2460d2fd95
Saadin Dassum <34032935+saadindassum@users.noreply.github.com>
2025-11-09 14:17:23 -0500

    Updates

--- src/javascript/flexible-poster.js
+++
@@ -1,6 +0,0 @@
-// for customized video - removes the poster element when the video loads.
-let vid = document.getElementById("video-banner");
-let poster = document.getElementById("fallback-banner");
-vid.onloadeddata = function () {
-    document.querySelector('#fallback-banner').style.display = 'none';
-};
