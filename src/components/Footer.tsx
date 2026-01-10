4011961429ebc4802ba695d11de54dbb7fbf73e0
Saadin Dassum <34032935+saadindassum@users.noreply.github.com>
2026-01-10 12:46:23 -0500

    Updated footer year
    Now it automatically stays up to date.

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 
 const Footer: React.FC = () => {
+  const currentYear = new Date().getFullYear();
   return (
     <footer>
       <img src="/assets/fullSD.png" alt="Saadin Dassum logo" />
       <span>
         Created by
         <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"> Saadin Dassum </a>
-        <span className="far fa-copyright">2025 All Rights Reserved.</span>
+        <span className="far fa-copyright">{currentYear} All Rights Reserved.</span>
       </span>
       <br />
     </footer>
